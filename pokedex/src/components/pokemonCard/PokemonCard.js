e7b2ce0babffd5001ab1bba39d6eac5585de0912
carolinabispo <carolinabispos07@gmail.com>
2023-07-09 16:53:10 -0300

    criado componentes

---
+++ pokedex/src/components/pokemonCard/PokemonCard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PokemonCard = () => {
+  return (
+    <div>
+      <h2>Pokemon Card</h2>
+    </div>
+  )
+}
+
+export default PokemonCard
---
+++ pokedex/src/pages/PokemonListPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PokemonListPage = () => {
+  return (
+    <div>
+      <h2>pokemon list page</h2>
+    </div>
+  )
+}
+
+export default PokemonListPage
