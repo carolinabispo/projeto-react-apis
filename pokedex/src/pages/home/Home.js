8aabbd02badd8b1a60c885ccca0f37bb3fc319d1
carolinabispo <carolinabispos07@gmail.com>
2023-07-23 13:11:18 -0300

    feito rotas com react router

--- pokedex/src/pages/home/Home.js
+++ pokedex/src/pages/home/Home.js
@@ -1,28 +1,40 @@
-import React from 'react'
-import Header from '../../components/header/Header'
-import '../home/style.css'
-import PokemonCard from '../../components/pokemonCard/PokemonCard'
+import React from "react";
+import Header from "../../components/header/Header";
+import "../home/style.css";
+import PokemonCard from "../../components/pokemonCard/PokemonCard";
 const Home = () => {
   return (
-    <div className='div_home'>
-      <div>
-        <Header/>
+    <div>
+      <div className="div_home">
+        <div>
+          <Header />
+        </div>
+        <div className="card">
+          <div className="title">
+            <p>Todos os pokémons</p>
+          </div>
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+          <PokemonCard />
+        </div>
       </div>
-     <div className='card'>
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
-      <PokemonCard />
-
-     </div>
-    
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
